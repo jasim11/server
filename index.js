9ae84bf582f93ac4621d7757da87bb0c28c91e2a
jasim11 <51187260+jasim11@users.noreply.github.com>
2022-03-22 16:48:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use("/user", userRouter);
 const CONNECTION_URL = 'mongodb+srv://master123:123@cluster0.vttub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
 
